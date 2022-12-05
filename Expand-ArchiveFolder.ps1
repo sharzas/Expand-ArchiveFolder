0d79e787667a787fd6680e24cce5d67af8d25d25
sharzas <sharzas@gmail.com>
2022-12-05 20:06:49 +0100

    Fixed issues with paths and added -WhatIf support

--- Expand-ArchiveFolder.ps1
+++ Expand-ArchiveFolder.ps1
@@ -27,19 +27,20 @@
     any directory under -Path containing an archive to extract.
 #>
 
-[CmdletBinding()]
+[CmdletBinding(SupportsShouldProcess=$true)]
 
 param (
-    $Path = (Get-Location).Path,
-    $Destination = "D:\Data\Downloads\Torrent\Extract"
+    [Object]$Path = (Get-Location).Path,
+    [Object]$Destination = "D:\Data\Downloads\Torrent\Extract"
 )
 
 if (!(Test-Path -Path $Path -PathType Container)) {
     throw ('Source Path "{0}" does not exist' -f $Path)
 
+} else {
     try {
         # Get source path as DirectoryInfo object
-        Get-Item -Path $SourcePath
+        $Path = Get-Item -Path $Path
     } catch {
         Write-Warning ('Unable to read directory "{0}"' -f $Path)
         Write-Host
@@ -53,21 +54,33 @@ if (!(Test-Path -Path $Destination -PathType Container)) {
 
 
 # Construct the full extract destination, by adding the root directory to the path
-$ExtractPath = Join-Path -Path $Destination -ChildPath ($SourcePath | Split-Path -Leaf)
+$ExtractPath = Join-Path -Path $Destination -ChildPath ($Path.Fullname | Split-Path -Leaf)
 
-Write-Host ('Destination Extract Path: "{0}"' -f $ExtractPath)
+Write-Host -ForegroundColor Yellow ('Destination Extract Path: "{0}"' -f $ExtractPath)
+Write-Host ""
 
 # Enumerate all archives in folder structure - make sure to
 # exclude everything but the main archive from multipart archives
-$Archives = $SourcePath | Get-ChildItem -Recurse -File| where-object {($_.name-match '\.rar$|\.zip$|\.7z$') -and ($_.name -notmatch '\.(part1\d+?|part[2-9]*?)\.rar$' )}
+$Archives = @($Path | Get-ChildItem -Recurse -File| where-object {($_.name-match '\.rar$|\.zip$|\.7z$') -and ($_.name -notmatch '\.(part1\d+?|part[2-9]*?)\.rar$' )})
+
+if ($Archives.Count -lt 1) {
+    Write-Host -ForegroundColor Green ('Nothing to do - no archives found in path "{0}"' -f $Path)
+    break
+}
+
 
 # Create the list of archives and the constructed destination
 $ExpandList = $Archives | Foreach-Object {
     [PSCustomObject]@{
         Archive = $_
-        Destination = (Join-Path -Path $ExtractPath -ChildPath $_.Directory.FullName.Replace($SourcePath.Fullname, "")) -replace "\\$",""
+        Destination = (Join-Path -Path $ExtractPath -ChildPath $_.Directory.FullName.Replace($Path.Fullname, "")) -replace "\\$",""
     }
 } #| select -first 1
+
+if ($WhatIfPreference) {    
+    $ExpandList | Format-Table -Auto | Out-String -Stream | Foreach-Object { Write-Host ('WhatIf: {0}' -f  $_) }
+    break
+}
 
 
 # Loop through the list, and expand them
